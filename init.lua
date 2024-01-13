db5d58a8f7e6154b3de57621f1b68c1651757d89
Rodrigo Medeiros <roddmedeiross@gmail.com>
2024-01-12 21:15:58 -0300

    added noise and change dap commands

--- init.lua
+++ init.lua
@@ -293,7 +293,7 @@ vim.defer_fn(function()
     ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'ruby', 'ocaml', 'haskell' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
+    auto_install = true,
 
     highlight = { enable = true },
     indent = { enable = true },
